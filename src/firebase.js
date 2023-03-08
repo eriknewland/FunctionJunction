8122587c37ffbe4366a07d0139c5fe243d2a2c2b
eriknewland <114263701+eriknewland@users.noreply.github.com>
2023-03-08 12:49:57 -0800

    ...

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/compat/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const app = firebase.initializeApp({
-  apiKey: 'AIzaSyBu5WyYomNvWKVnDp0FOAXj8sbWg86Bpik',
-  authDomain: 'functionjunction-dev.firebaseapp.com',
-  databaseURL: 'https://functionjunction-dev-default-rtdb.firebaseio.com',
-  projectId: 'functionjunction-dev',
-  storageBucket: 'functionjunction-dev.appspot.com',
-  messagingSenderId: '876555321057',
-  appId: '1:876555321057:web:4ffb8a00c81ad6ee4441a0',
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 });
 
 export const auth = app.auth();
